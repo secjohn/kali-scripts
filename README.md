84910dbc4a1e75e38e3f6c453dc31b66b25d7d1e
John Woods <zenbanjoman@gmail.com>
2013-08-31 20:25:25 -0500

    Complete update of kaliupdate.sh script and updated README to reflect the changes.

--- README.md
+++ README.md
@@ -4,14 +4,11 @@
 This is simply a place for me to store scripts to make my life easier using kali.  Hopfully others will find them useful and even contribute to them.
 
 ### kaliupdate.sh
-This is a simple update script, I purposfully kept it simple and easy to read with a lot of echo's to the screen so people newer to shell scripting could understand it and easily update it adding the tools they want to keep updated.  Simply it:
+Update 8/31/12:
+I rewrote the update script.  People haven't been adding to it so I decided to stop keeping it simple and make it more flexible.  I added switches so you can decide if you just want to update packages, add packages, add software from source, or all of it.  To get the same functionality as before just add a -a to the end of the script.
 
-1. Updates the system
-2. Updates a few tools I use often enough to want to keep up to date.
+I remove the Nessus update since that doesn't seem to work anymore with Nessus 5.  I added aircrack-ng install from source since it does really seem needed if you are using airobase-ng for evil twin attacks.  
 
-I didn't automate adding in bleeding edge packages.  I'm going to wait and see how that goes first.  But this is a lot less than the backtrack update script I wrote.  Kali is doing a good job keeping things updated often in packages, no need to do more.
-
-I hope someone out there finds it useful to see and I hope people contribute back and I would love to see what tools other people like.
 
 ### chromium_fix.sh
-I like using Chrome, and I like running as root on Kali.  I hate having to edit the chromium file every time I do an apt-get upgrade so it runs.  So I made this script which will fix it for me when it is updated.  I'm using the chromium package from apt-get install chromium.  If you download Chrome from google and install it, the file name is /usr/bin/chrome and the exec line which is replaced is different.  So the script won't work for that one.  Ditch it and install the offical package, it isn't updated every day like the other one too, so it is less annoying.  If a few people ask I'll make a script for that one too.
+I like using Chrome, and I like running as root on Kali.  I hate having to edit the chromium file every time I do an apt-get upgrade so it runs.  So I made this script which will fix it for me when it is updated.  I'm using the chromium package from apt-get install chromium (now in the extra packages option of the update script) since it seems to be updated far less than the chrome package from google.  If you download Chrome from google and installed it, the file name is /usr/bin/chrome and the exec line which is replaced is different, threfore this script won't work for that one.  Ditch it and install Chromium.  It isn't updated every day like the other one too, so it is less annoying.
--- kaliupdate.sh
+++ kaliupdate.sh
@@ -1,68 +1,89 @@
-#!/bin/bash
+#This function shows which switches are needed to run the script.
+usage(){
+	echo "Use one or more of the following switches"
+	echo " -p Upgrade packages"
+	echo " -e Install select extra packages"
+	echo " -s Install select tools from source"
+	echo " -a All, same as -pes"
+	echo " -h see this message"
+}
 
-echo -e "\e[1;34m[*] Checking connectivity\e[0m"
-echo
-sleep 2
-
-# Variables
-TESTHOST="google.com"
-TEST=`ping -c1 $TESTHOST | grep "1 packets transmitted" | cut -d "," -f3`
-PASS=" 0% packet loss"
-
-if [ "$TEST" = "$PASS" ]; then
-        echo -e "\e[1;34m[*] Congratz bro! t3h Interntz is working.\e[0m"
-	sleep 3
-	echo
+#This option updates the packages installed and Metasploit.
+package(){
 	echo -e "\e[1;34m[*] Updating the entire system.\e[0m"
-	echo
-	apt-get update && apt-get upgrade -y
-	sleep 2
-	echo -e "\e[1;34m[*]Adding some needed packages.\e[0m"
-	apt-get -y install autoconf
-	sleep 2
-	echo -e "\e[1;34m[*]Updating MSF.\e[0m"
-	/opt/metasploit/apps/pro/msf3/msfupdate
-	cd /opt/metasploit/apps/pro/msf3/
-    bundle install
-    echo
-	sleep 2
-#	This worked for Nessus 4, looks like Nessus 5 doesn't have a command line option, go click the button instead.
-#	echo -e "\e[1;34m[*]Updating Nessus.\e[0m"
-#
-#	if [ -f /opt/nessus/sbin/nessus-update-plugins ]; then
-#		cd /opt/nessus/sbin
-#		./nessus-update-plugins
-#	else
-#		echo -e "\e[1;34m[*]Install Nessus and try again, or not.\e[0m"
-#	fi
+  apt-get update && apt-get upgrade -y
+  echo -e "\e[1;34m[*]Updating MSF.\e[0m"
+  /opt/metasploit/apps/pro/msf3/msfupdate
+  cd /opt/metasploit/apps/pro/msf3/
+  bundle install
+}
 
-	echo
-	sleep 2
-	echo -e "\e[1;34m[*]Installing SVN version of fuzzdb in /usr/share/fuzzdb and keeping it updated.\e[0m"
+#This installs extra packages I find most useful to add to a fresh Kali install.
+extra(){
+  echo -e "\e[1;34m[*]Adding some needed and nice packages.\e[0m"
+  apt-get -y install autoconf terminator libssl-dev hostapd ipcalc isc-dhcp-server chromium
 
-	if [ -d /usr/share/fuzzdb ]; then
-		cd /usr/share/fuzzdb
-		svn up
-	else
-		echo -e "\e[1;34m[*]Fuzzdb not found, installing at /usr/share/fuzzdb.\e[0m"
-		cd /usr/share
-		svn co http://fuzzdb.googlecode.com/svn/trunk fuzzdb
-	fi
+}
 
-	echo
-	sleep 2
-	echo -e "\e[1;34m[*]Adding nmap-svn to /opt/nmap-svn.\e[0m"
+#This section installs software from source.
+#Fuzzdb is downloaded from svn
+#Nmap is build in the /opt/nmap-svn dir but it is not installed over the package, so you have to call it manually
+#Aircrack-ng is downloaded and compiled and it is installed over the package, I've found airobase-ng to be buggy otherwise.
 
-	svn co --username guest --password "" https://svn.nmap.org/nmap /opt/nmap-svn
-	cd /opt/nmap-svn
-	./configure && make
-	/opt/nmap-svn/nmap -V
-	echo "Installed in /opt/nmap-svn"
-	echo
-	sleep 2
-	echo -e "\e[1;34m[*]Installed or updated Fuzzdb to /usr/share/fuzzdb.\e[0m"
-	echo -e "\e[1;34m[*]Installed or updated nmap-svn to /opt/nmap-svn.\e[0m"
-	echo -e "\e[1;34m[*]All done.\e[0m"
-else
-        echo -e "\e[1;34m[*] You do not have an Internet connection.\e[0m"
-fi
+source(){
+  echo -e "\e[1;34m[*]Making sure packages required to build things are there.\e[0m"
+  apt-get -y install autoconf libssl-dev
+  echo -e "\e[1;34m[*]Installing SVN version of fuzzdb in /usr/share/fuzzdb and keeping it updated.\e[0m"
+  if [ -d /usr/share/fuzzdb ]; then
+    cd /usr/share/fuzzdb
+    svn up
+  else
+    echo -e "\e[1;34m[*]Fuzzdb not found, installing at /usr/share/fuzzdb.\e[0m"
+    cd /usr/share
+    svn co http://fuzzdb.googlecode.com/svn/trunk fuzzdb
+  fi
+  echo -e "\e[1;34m[*]Adding nmap-svn to /opt/nmap-svn.\e[0m"
+  svn co --username guest --password "" https://svn.nmap.org/nmap /opt/nmap-svn
+  cd /opt/nmap-svn
+  ./configure && make
+  /opt/nmap-svn/nmap -V
+  echo "Installed in /opt/nmap-svn"
+  echo -e "\e[1;34m[*]Updating aircrack-ng from SVN.\e[0m"
+  if [ -d /opt/aircrack-ng-svn ]; then
+    cd /opt/aircrack-ng-svn
+    svn up
+  else
+    svn co http://svn.aircrack-ng.org/trunk/ /opt/aircrack-ng-svn
+    cd /opt/aircrack-ng-svn
+  fi
+  make && make install
+  airodump-ng-oui-update
+  #Saying what happened:
+  echo -e "\e[1;34m[*]Installed or updated Fuzzdb to /usr/share/fuzzdb.\e[0m"
+  echo -e "\e[1;34m[*]Installed or updated nmap-svn to /opt/nmap-svn.\e[0m"
+  echo -e "\e[1;34m[*]Downloaded svn version of aircrack-ng to /opt/aircrack-ng-svn and overwrote package with it.\e[0m"
+  sleep 5
+}
+
+#This displays the usage info if you didn't put in a switch.
+(($# )) || usage
+
+#This is the main logic calling the proper functions based on the switches used.
+while getopts "pesah" flag
+do
+  if [ "$flag" = "h" ]; then
+  	usage
+  elif [ "$flag" = "p" ]; then
+  	package
+  elif [ "$flag" = "e" ]; then
+  	extra
+  elif [ "$flag" = "s" ]; then
+  	source
+  elif [ "$flag" = "a" ]; then
+  	package
+  	extra
+  	source
+  else
+  	usage
+  fi
+done
